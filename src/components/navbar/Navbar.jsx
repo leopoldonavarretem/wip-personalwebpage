e4f9073dae1a381a982e47d7155debebe0d368d9
Leopoldo Navarrete <Leopoldo12@outlook.com>
2022-06-08 20:28:13 -0700

    Added Navigation Bar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,92 +5,49 @@ import { useState } from "react";
 
 //Import componenets
 import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
 import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
-import MenuIcon from "@mui/icons-material/Menu";
-
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
 
 function Navbar(props) {
-  const [anchorElNav, setAnchorElNav] = useState(null);
-  const [anchorElUser, setAnchorElUser] = useState(null);
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
   return (
-    <AppBar position="static">
+    <AppBar color="primary" position="static">
       <Container maxWidth="xl">
-        <Toolbar disableGutters >
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              color="inherit"
-              onClick={handleOpenNavMenu}
-            >
-              <MenuIcon />
-            </IconButton>
+        <Toolbar>
+        <Link to="/" style={{textDecoration: "none", color:"white"}}>
+                <Typography color="inherit" variant="title">
+                  Leopoldo Navarrete
+                </Typography>
+              </Link>
 
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-
-
-                <MenuItem onClick={handleCloseNavMenu}>
-                  <Link to="/portfolio" style={{textDecoration: "none"}}>
-                  <Typography textAlign="center" sx={{color: "green"}}>Portfolio</Typography>
-                  </Link>
-                </MenuItem>
-
-                <MenuItem onClick={handleCloseNavMenu}>
-                  <Link to="/aboutme" style={{textDecoration: "none"}}>
-                  <Typography textAlign="center" sx={{color: "green"}}>About Me</Typography>
-                  </Link>
-                </MenuItem>
-
-                <MenuItem onClick={handleCloseNavMenu}>
-                  <Link to="/contact" style={{textDecoration: "none"}}>
-                  <Typography textAlign="center" sx={{color: "green"}}>Contact</Typography>
-                  </Link>
-                </MenuItem>
-              
-            </Menu>
-          </Box>
-
+          <List component="nav">
+            <ListItem component="div">
+              <ListItemText inset>
+              <Link to="/portfolio" style={{textDecoration: "none", color:"white"}}>
+                <Typography color="inherit" variant="title">
+                  Portfolio
+                </Typography>
+              </Link>
+              </ListItemText>
+              <ListItemText inset>
+              <Link to="/aboutme" style={{textDecoration: "none", color:"white"}}>
+                <Typography color="inherit" variant="title">
+                  About Me
+                </Typography>
+              </Link>
+              </ListItemText>
+              <ListItemText inset>
+              <Link to="/contact" style={{textDecoration: "none", color:"white"}}>
+                <Typography color="inherit" variant="title">
+                  Contact
+                </Typography>
+              </Link>
+              </ListItemText>
+            </ListItem>
+          </List>
         </Toolbar>
       </Container>
     </AppBar>
