c69ba61eee4e2a2c2c8695790d07eb77e9f532f4
Leopoldo Navarrete <leopoldonavarretem@outlook.com>
2022-09-07 12:42:27 -0700

    Finished Contact Form.

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,14 +1,33 @@
 //MUI Import
 import { Grid } from "@mui/material";
 import "./ContactForm.css";
+import { useState } from "react";
 
 function ContactForm(props) {
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  const href = `mailto:Leopoldonavarretem@outlook.com?subject=${subject}&Body=${message}`;
+
+  function handleSubjectChange(event) {
+    return setSubject(event.target.value);
+  }
+
+  function handleMessageChange(event) {
+    return setMessage(event.target.value);
+  }
+
   return (
     <Grid item xl={6} xs={12}>
-      <form action="" className="contactForm">
+      <form className="contactForm">
         <h3 className="Contact">Email</h3>
-        <label for="name">Subject</label>
-        <input type="text" id="name" name="name" className="input" />
+        <label>Subject</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          className="input"
+          onChange={handleSubjectChange}
+        />
         <label>Message</label>
         <textarea
           type="text"
@@ -16,8 +35,12 @@ function ContactForm(props) {
           name="message"
           className="input"
           rows="4"
+          onChange={handleMessageChange}
         />
-        <button type="submit">Send.</button>
+
+        <a href={href} className="ContactSend">
+          Send
+        </a>
       </form>
     </Grid>
   );
