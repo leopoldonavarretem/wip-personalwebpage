e2dcf2814bcc33cdc3371fa88770b59806ebb87d
Leopoldo Navarrete <leopoldonavarretem@outlook.com>
2022-09-06 20:10:16 -0700

    CSS and HTML complete for Contact Form.

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -4,15 +4,20 @@ import "./ContactForm.css";
 
 function ContactForm(props) {
   return (
-    <Grid item xl={6}>
+    <Grid item xl={6} xs={12}>
       <form action="" className="contactForm">
-        <label for="name">Name</label>
-        <input type="text" id="name" name="name" />
-        <label for="email">Email</label>
-        <input type="email" id="email" name="email" />
+        <h3 className="Contact">Email</h3>
+        <label for="name">Subject</label>
+        <input type="text" id="name" name="name" className="input" />
         <label>Message</label>
-        <input type="text" id="message" name="message" height="40"/>
-        <button type="submit">Submit!</button>
+        <textarea
+          type="text"
+          id="message"
+          name="message"
+          className="input"
+          rows="4"
+        />
+        <button type="submit">Send.</button>
       </form>
     </Grid>
   );
