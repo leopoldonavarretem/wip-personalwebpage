6976656a8f7953f92a363c118dffb83fcb017c63
Leopoldo Navarrete <leopoldonavarretem@outlook.com>
2022-09-12 22:14:50 -0700

    bugs fixed

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -3,27 +3,39 @@ import { Grid } from "@mui/material";
 import "./ContactForm.css";
 
 function ContactForm(props) {
- 
   return (
     <Grid item xl={6} xs={12}>
-      <form
-        className="contactForm"
-        name="contact"
-        method="POST"
-        netlify
-      >
-      <input type="hidden" name="contact-form" value="contact" />
+      <form className="contactForm" name="contact" method="post">
+        <input type="hidden" name="contact-form" value="contact" />
 
-        <label className="contactLabel" htmlFor="fullName">Name</label>
-        <input type="text" id="fullName" name="fullName" className="contactInput" />
+        <label className="contactLabel" htmlFor="fullName">
+          Name
+        </label>
+        <input
+          type="text"
+          id="fullName"
+          name="fullName"
+          className="contactInput"
+        />
 
-        <label className="contactLabel" htmlFor="email">Email</label>
+        <label className="contactLabel" htmlFor="email">
+          Email
+        </label>
         <input type="email" id="email" name="email" className="contactInput" />
 
-        <label className="contactLabel" htmlFor="subject">Subject</label>
-        <input type="text" id="subject" name="subject" className="contactInput" />
+        <label className="contactLabel" htmlFor="subject">
+          Subject
+        </label>
+        <input
+          type="text"
+          id="subject"
+          name="subject"
+          className="contactInput"
+        />
 
-        <label className="contactLabel" htmlFor="message">Message</label>
+        <label className="contactLabel" htmlFor="message">
+          Message
+        </label>
         <textarea
           type="text"
           id="message"
